c444373c152286895a91c52e5dc9841b8512a7ee
christopher-w-murphy <murphtron5000@gmail.com>
2019-01-04 11:38:10 -0500

    updated analysis

--- deep.py
+++ deep.py
@@ -2,16 +2,14 @@ import numpy as np
 import pandas as pd
 from time import time
 
-from sklearn.metrics import average_precision_score
+from sklearn.metrics import average_precision_score, roc_auc_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.utils import class_weight
 
 from keras.callbacks import EarlyStopping
 from keras.models import load_model
 
 from imblearn.keras import balanced_batch_generator
-from imblearn.under_sampling import NearMiss
 
 from src.focal_loss import focal_loss
 from src.keras_model import build_model
@@ -41,6 +39,9 @@ X_tr_dnn = (scaler_dnn
 X_te_dnn = (scaler_dnn
             .transform(X_te))
 
+early_stopping = EarlyStopping(monitor='loss',
+                               patience=10)
+
 # Deep Learning
 
 APs_dnn = dict()
@@ -49,12 +50,10 @@ keras_model_1 = load_model('results/keras_model_50epochs.h5')
 t1 = time()
 keras_model_1.fit(X_tr_dnn,
                   y_tr,
-                  epochs=200,
+                  epochs=100,
                   initial_epoch=50,
                   batch_size=50,
-                  verbose=1,
-                  callbacks=[EarlyStopping(monitor='val_loss', patience=20)],
-                  validation_split=0.2)
+                  callbacks=[early_stopping])
 print('DNN finished in %0.1f seconds' %(time() - t1))
 probas_dnn_1 = keras_model_1.predict_proba(X_te_dnn)
 ap_dnn_1 = average_precision_score(y_te, probas_dnn_1)
@@ -63,20 +62,14 @@ print('DNN: AP = %0.3f' %APs_dnn['DNN'])
 pkl_save_obj(APs_dnn, 'APs_dnn')
 keras_model_1.save('results/keras_model_200epochs.h5')
 
-
-class_weights = class_weight.compute_class_weight('balanced',
-                                                  np.unique(y_tr),
-                                                  y_tr)
 keras_model_2 = build_model()
 t1 = time()
 keras_model_2.fit(X_tr_dnn,
                   y_tr,
-                  epochs=2,
-                  batch_size=200,
-                  verbose=1,
-                  callbacks=[EarlyStopping(monitor='val_loss', patience=20)],
-                  validation_split=0.2,
-                  class_weight=class_weights)
+                  epochs=100,
+                  batch_size=50,
+                  callbacks=[early_stopping],
+                  class_weight={0:1, 1:3})
 print('DNN w/ balanced weights finished in %0.1f seconds' %(time() - t1))
 probas_dnn_2 = keras_model_2.predict_proba(X_te_dnn)
 ap_dnn_2 = average_precision_score(y_te, probas_dnn_2)
@@ -84,15 +77,14 @@ APs_dnn['DNN w/ balanced weights'] = ap_dnn_2
 print('DNN w/ balanced weights: AP = %0.3f' %APs_dnn['DNN w/ balanced weights'])
 pkl_save_obj(APs_dnn, 'APs_dnn')
 
-keras_model_3 = build_model(loss_function=focal_loss())
+keras_model_3 = build_model(loss_function=focal_loss(gamma=0.5,
+                                                     alpha=0.5))
 t1 = time()
 keras_model_3.fit(X_tr_dnn,
                   y_tr,
-                  epochs=2,
-                  batch_size=200,
-                  verbose=1,
-                  callbacks=[EarlyStopping(monitor='val_loss', patience=20)],
-                  validation_split=0.2)
+                  epochs=100,
+                  batch_size=50,
+                  callbacks=[early_stopping])
 print('DNN w/ Focal Loss finished in %0.1f seconds' %(time() - t1))
 probas_dnn_3 = keras_model_3.predict_proba(X_te_dnn)
 ap_dnn_3 = average_precision_score(y_te, probas_dnn_3)
@@ -104,15 +96,13 @@ keras_model_4 = build_model()
 t1 = time()
 training_generator, steps_per_epoch = balanced_batch_generator(X_tr_dnn,
                                                                y_tr,
-                                                               sampler=NearMiss(),
                                                                batch_size=50)
 print('Balanced Batch Generation took %0.1f seconds' %(time() - t1))
 t1 = time()
 keras_model_4.fit_generator(generator=training_generator,
                             steps_per_epoch=steps_per_epoch,
-                            epochs=1300,
-                            verbose=1,
-                            callbacks=[EarlyStopping(monitor='loss', patience=20)])
+                            epochs=100,
+                            callbacks=[early_stopping])
 print('DNN w/ balanced batches finished in %0.1f seconds' %(time() - t1))
 probas_dnn_4 = keras_model_4.predict_proba(X_te_dnn)
 ap_dnn_4 = average_precision_score(y_te, probas_dnn_4)
--- src/compute_LL.py
+++ src/compute_LL.py
@@ -3,7 +3,13 @@ from scipy.stats import rv_histogram
 
 class logLikelihood():
     def __init__(self):
-        self.mus = np.linspace(0.0, 0.12, 100)
+        """
+        mus: values of mu to test
+        rescale: correction factor to report significance for 300/fb of data
+        rescale = n_test_events / (sigma(jjWpmWpm) * Br(W->lnu)^2 * (300/fb)) = 4.67
+        """
+        self.mus = np.linspace(0.0, 0.12, 200)
+        self.rescale = 32000 / (213.3 * 0.3272**2 * 300)
 
     def hist_dist(self, data):
         """
@@ -24,7 +30,7 @@ class logLikelihood():
 
     def compute_log_likelihood(self, clf, X, y):
         """
-        given a classifier and labeled data, compute the associated log-likelihood as function of mu
+        given a classifier and labeled data, compute the associated log-likelihood as a function of mu
         """
         probas_all = clf.predict_proba(X)
         probas_1 = clf.predict_proba(X[y == 1])
@@ -42,3 +48,16 @@ class logLikelihood():
         pdf_0 = hd_0.pdf(probas_all)
 
         return np.array([self.mLL_mu(mu, pdf_1, pdf_0) for mu in self.mus])
+
+    def compute_sigma(self, m_likelihood, threshold=0.5):
+        """
+        estimate the number of standard deviations from zero this measurement yields
+        """
+        arg_cen = np.argmin(m_likelihood)
+        central = self.mus[arg_cen]
+
+        delta_likelihood = m_likelihood - np.min(m_likelihood)
+        arg_std = np.argmin((delta_likelihood[:arg_cen] - threshold)**2)
+        stdev = central - self.mus[arg_std]
+
+        return central / stdev
